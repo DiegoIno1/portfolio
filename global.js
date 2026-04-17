7b58603dfa702f68dd2bec1fc82ba7eee772fa39
DiegoIno1 <dinostroza@ucsd.edu>
2026-04-17 04:37:05 -0700

    automatic current link

--- global.js
+++ global.js
@@ -3,3 +3,15 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+let navLinks = $$("nav a");
+
+// Step 2.2: Find the link to the current page
+let currentLink = navLinks.find(
+  (a) => a.host === location.host && a.pathname === location.pathname,
+);
+
+if (currentLink) {
+  // or if (currentLink !== undefined)
+  currentLink.classList.add('current');
+}

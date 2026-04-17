c42a0ae3d7f601cefe37880e1116e6aa9e7f52a8
DiegoIno1 <dinostroza@ucsd.edu>
2026-04-17 04:56:14 -0700

    small fix

--- global.js
+++ global.js
@@ -32,12 +32,13 @@ for (let p of pages) {
   let url = p.url;
   let title = p.title;
   // next step: create link and add it to nav
-  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
-
+  
   const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
   ? "/"                  // Local server
   : "/website/";         // GitHub Pages repo name
 
   url = !url.startsWith('http') ? BASE_PATH + url : url;
+
+  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
 }
 

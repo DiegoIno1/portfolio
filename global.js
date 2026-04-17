af4cb9af65bd516ca623e8d4dfd0c2849b85d5b9
DiegoIno1 <dinostroza@ucsd.edu>
2026-04-17 05:00:12 -0700

    another fix

--- global.js
+++ global.js
@@ -28,17 +28,18 @@ let pages = [
 let nav = document.createElement('nav');
 document.body.prepend(nav);
 
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/website/";         // GitHub Pages repo name
+
 for (let p of pages) {
   let url = p.url;
   let title = p.title;
   // next step: create link and add it to nav
   
-  const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
-  ? "/"                  // Local server
-  : "/website/";         // GitHub Pages repo name
-
   url = !url.startsWith('http') ? BASE_PATH + url : url;
-
   nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
 }
+
+
 

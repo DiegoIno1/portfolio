3d6e370e111061396430edde17febe967e5cc16e
DiegoIno1 <dinostroza@ucsd.edu>
2026-04-17 19:48:31 -0700

    fixed colors?

--- global.js
+++ global.js
@@ -72,4 +72,5 @@ document.body.insertAdjacentHTML(
 const select = document.querySelector('.color-scheme select');
 select.addEventListener('input', function (event) {
   console.log('color scheme changed to', event.target.value);
+  document.documentElement.style.setProperty('color-scheme', event.target.value);
 });

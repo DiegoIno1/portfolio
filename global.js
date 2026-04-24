599e1e350e8ca3cdd47be801a8d2c55d5cd09bf1
DiegoIno1 <dinostroza@ucsd.edu>
2026-04-24 01:43:24 -0700

    step 1

--- global.js
+++ global.js
@@ -84,3 +84,37 @@ select.addEventListener('input', function (event) {
   localStorage.colorScheme = event.target.value;
   document.documentElement.style.colorScheme = event.target.value;
 });
+
+export async function fetchJSON(url) {
+  try {
+    // Fetch the JSON file from the given URL
+    const response = await fetch(url);
+    
+    if (!response.ok) {
+      throw new Error(`Failed to fetch projects: ${response.statusText}`);
+    }
+    
+    const data = await response.json();
+    console.log(response);
+    
+    return data;
+  } catch (error) {
+    console.error('Error fetching or parsing JSON data:', error);
+  }
+}
+
+export function renderProjects(projects, containerElement, headingLevel = 'h2') {
+  containerElement.innerHTML = '';
+  
+  projects.forEach(project => {
+    const article = document.createElement('article');
+    
+    article.innerHTML = `
+      <${headingLevel}>${project.title}</${headingLevel}>
+      <img src="${project.image}" alt="${project.title}">
+      <p>${project.description}</p>
+    `;
+    
+    containerElement.appendChild(article);
+  });
+}
